bd9727c44660b6469bec59c540a591bbc432d10e
Eric Oliveira <ericjmoliveira@gmail.com>
2022-03-15 15:31:53 -0300

    Implement style handling, improve some functions

--- src/App.js
+++ src/App.js
@@ -13,59 +13,110 @@ import bottomBlobSmall from "./assets/bottom-blob-small.png";
 import "./App.css";
 
 function App() {
-    // Backgrounds
+    // Design background images used
     const backgrounds = [
-        {
-            backgroundImage: `url(${topBlob}), url(${bottomBlob})`
-        },
-        {
-            backgroundImage: `url(${topBlobSmall}), url(${bottomBlobSmall})`
-        }
+        { backgroundImage: `url(${topBlob}), url(${bottomBlob})` },
+        { backgroundImage: `url(${topBlobSmall}), url(${bottomBlobSmall})` },
     ];
 
-    // Handle the necessary states
+    // States
     const [quiz, setQuiz] = useState(false);
+    const [rawData, setRawData] = useState([]);
     const [questionsList, setQuestionsList] = useState([]);
-    const [answers, setAnswers] = useState([
-        { user: "", correct: "" },
-        { user: "", correct: "" },
-        { user: "", correct: "" },
-        { user: "", correct: "" },
-        { user: "", correct: "" }
-    ]);
-    const [checkButton, setCheckButton] = useState(false);
+    const [userAnswers, setUserAnswers] = useState(["", "", "", ""]);
+    const [checkStart, setCheckStart] = useState(false);
     const [score, setScore] = useState(0);
-    const [newGame, setNewGame] = useState(true);
+    const [newGame, setNewGame] = useState(false);
 
-    // Fetch questions from the OTDB API
+    // Fetches the data from the OTDB API
     useEffect(() => {
         fetch("https://opentdb.com/api.php?amount=5&type=multiple")
             .then((response) => response.json())
-            .then((data) => setQuestionsList(data.results))
+            .then((data) => setRawData(data.results))
             .catch((error) => console.log(error));
     }, [newGame]);
+
+    // Manipulates the data received, shuffles the options, decodes the texts
+    useEffect(() => {
+        function shuffleOptions() {
+            let newQuestionList = [...rawData];
+
+            for (let i = 0; i < newQuestionList.length; i++) {
+                let newQuestionObj = { ...newQuestionList[i] };
+                newQuestionObj.options = newQuestionObj.incorrect_answers;
+
+                const randomIndex = Math.floor(Math.random() * 4);
+                newQuestionObj.options.splice(randomIndex, 0, newQuestionObj.correct_answer);
+
+                newQuestionObj.question = decodeEntities(newQuestionObj.question);
+                newQuestionObj.options = newQuestionObj.options.map((option) =>
+                    decodeEntities(option)
+                );
+                newQuestionObj.correct_answer = decodeEntities(newQuestionObj.correct_answer);
+
+                newQuestionList[i] = newQuestionObj;
+            }
+
+            console.log(newQuestionList);
+
+            setQuestionsList(newQuestionList);
+        }
+
+        shuffleOptions();
+    }, [rawData]);
+
+    // Decodes HTML entities in the text given
+    function decodeEntities(text) {
+        const textArea = document.createElement("textArea");
+        textArea.innerHTML = text;
+        return textArea.value;
+    }
+
+    // Styles to be applied in the option buttons
+    const optStyles = [
+        { backgroundColor: "inherit", border: "1px solid #4d5b9e" }, // Default
+        { backgroundColor: "#d6dBf5", border: "none" }, // Selected
+        { backgroundColor: "#94d7a2", border: "none" }, // Correct
+        { backgroundColor: "#f8bcbc", border: "none" }, // Wrong
+        { backgroundColor: "inherit", border: "1px solid #4d5b9e", opacity: 0.5 }, // None
+    ];
+
+    // Handles the style of the option buttons
+    function setStyling(index, opt) {
+        // Boolean variables to be used in the conditionals
+        const defaultCheck = !checkStart && userAnswers[index] === "";
+        const selectedCheck = !checkStart && userAnswers[index] === opt;
+        const correctCheck = checkStart && questionsList[index].correct_answer === opt;
+        const wrongCheck =
+            checkStart && questionsList[index].correct_answer !== opt && opt === userAnswers[index];
+        const noneAboveCheck =
+            checkStart && opt !== userAnswers[index] && opt !== questionsList[index].correct_answer;
+
+        if (defaultCheck) return optStyles[0];
+        if (selectedCheck) return optStyles[1];
+        if (correctCheck) return optStyles[2];
+        if (wrongCheck) return optStyles[3];
+        if (noneAboveCheck) return optStyles[4];
+    }
 
     // Switches to the quiz page
     function startQuiz() {
         setQuiz((prevQuiz) => !prevQuiz);
     }
 
-    // Gets answers from the questions components
-    function getAnswers(index, userAnswer, correctAnswer) {
-        let newAnswers = [...answers];
-        let answerPair = { ...newAnswers[index] };
-        answerPair.user = userAnswer;
-        answerPair.correct = correctAnswer;
-        newAnswers[index] = answerPair;
+    // Gets user answer from the questions components
+    function getUserAnswer(index, userOption) {
+        const newUserAnswers = [...userAnswers];
+        newUserAnswers[index] = userOption;
 
-        setAnswers(newAnswers);
+        setUserAnswers(newUserAnswers);
     }
 
-    // Checks the score
+    // Calculates the user score
     function checkScore() {
-        setCheckButton((prevCheckButton) => !prevCheckButton);
-        for (let i = 0; i < answers.length; i++) {
-            if (answers[i].user === answers[i].correct) {
+        setCheckStart((prevCheckButton) => !prevCheckButton);
+        for (let i = 0; i < userAnswers.length; i++) {
+            if (userAnswers[i] === questionsList[i].correct_answer) {
                 setScore((prevScore) => prevScore + 1);
             }
         }
@@ -74,7 +125,7 @@ function App() {
     // Restarts the game
     function playAgain() {
         setNewGame((prevNewGame) => !prevNewGame);
-        setCheckButton((prevCheckButton) => !prevCheckButton);
+        setCheckStart((prevCheckButton) => !prevCheckButton);
         setScore(0);
     }
 
@@ -88,13 +139,13 @@ function App() {
                         key={uuidv4()}
                         index={questionsList.indexOf(question)}
                         title={question.question}
-                        correctOption={question.correct_answer}
-                        incorrectOptions={question.incorrect_answers}
-                        handleAnswers={getAnswers}
+                        options={question.options}
+                        handleStyle={setStyling}
+                        handleAnswers={getUserAnswer}
                     />
                 );
             })}
-            {!checkButton ? (
+            {!checkStart ? (
                 <Button type="check-answers" handleClick={checkScore}>
                     Check answers
                 </Button>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,71 +1,40 @@
-import { useState } from "react";
 import "./Question.css";
 
-function Question(props) {
-    const questionTitle = decodeEntities(props.title);
-    const correctAnswer = decodeEntities(props.correctOption);
-
-    const [optStyles, setOptStyles] = useState([
-        { backgroundColor: "inherit", border: "1px solid #4d5b9e" },
-        { backgroundColor: "inherit", border: "1px solid #4d5b9e" },
-        { backgroundColor: "inherit", border: "1px solid #4d5b9e" },
-        { backgroundColor: "inherit", border: "1px solid #4d5b9e" },
-    ]);
-    const [options, setOptions] = useState(props.incorrectOptions.map((opt) => decodeEntities(opt)));
-    const [shuffle, setShuffle] = useState(true);
-
-    if (shuffle) shuffleOptions();
-
-    // Shuffles the options in order to display on the screen
-    function shuffleOptions() {
-        const shuffledOptions = [...options, correctAnswer];
-        shuffledOptions.sort(() => Math.random() - 0.5);
-        setOptions(shuffledOptions);
-        setShuffle(false);
-
-        console.log(correctAnswer);
-    }
-
-    // Highlights user option
-    function highlightUserOption(e) {
-        let newStyles = [
-            { backgroundColor: "inherit", border: "1px solid #4d5b9e" },
-            { backgroundColor: "inherit", border: "1px solid #4d5b9e" },
-            { backgroundColor: "inherit", border: "1px solid #4d5b9e" },
-            { backgroundColor: "inherit", border: "1px solid #4d5b9e" },
-        ];
-
-        let optChoosenStyle = { ...optStyles[e.target.id] };
-        optChoosenStyle.backgroundColor = "#d6dbf5";
-        optChoosenStyle.border = "none";
-        newStyles[e.target.id] = optChoosenStyle;
-
-        setOptStyles(newStyles);
-    }
-
-    // Decodes HTML entities that come from the API text
-    function decodeEntities(text) {
-        const textArea = document.createElement("textArea");
-        textArea.innerHTML = text;
-        return textArea.value;
+function Question({ index, title, options, handleStyle, handleAnswers, checkState }) {
+    // Send answers to the App component
+    function sendAnswers(e) {
+        const userOption = e.target.textContent;
+        handleAnswers(index, userOption);
     }
 
     return (
         <div className="question-container">
             <div className="title-container">
-                <h2 className="question-title">{questionTitle}</h2>
+                <h2 className="question-title">{title}</h2>
             </div>
             <div className="options-container">
-                <button id={0} onClick={highlightUserOption} style={optStyles[0]}>
+                <button
+                    onClick={!checkState ? sendAnswers : undefined}
+                    style={handleStyle(index, options[0])}
+                >
                     {options[0]}
                 </button>
-                <button id={1} onClick={highlightUserOption} style={optStyles[1]}>
+                <button
+                    onClick={!checkState ? sendAnswers : undefined}
+                    style={handleStyle(index, options[1])}
+                >
                     {options[1]}
                 </button>
-                <button id={2} onClick={highlightUserOption} style={optStyles[2]}>
+                <button
+                    onClick={!checkState ? sendAnswers : undefined}
+                    style={handleStyle(index, options[2])}
+                >
                     {options[2]}
                 </button>
-                <button id={3} onClick={highlightUserOption} style={optStyles[3]}>
+                <button
+                    onClick={!checkState ? sendAnswers : undefined}
+                    style={handleStyle(index, options[3])}
+                >
                     {options[3]}
                 </button>
             </div>
